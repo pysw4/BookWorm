f0e3e933adf4860d61ff4b1cb44cb6a308da1d83
Cemen <pingjingqing4@icloud.com>
2024-12-20 20:13:20 +0800

    Initial Commit

---
+++ BookWorm/BookWormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormApp.swift
+//  BookWorm
+//
+//  Created by e7 on 2024-12-20.
+//
+
+import SwiftUI
+
+@main
+struct BookWormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookWormTests/BookWormTests.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormTests.swift
+//  BookWormTests
+//
+//  Created by e7 on 2024-12-20.
+//
+
+import Testing
+@testable import BookWorm
+
+struct BookWormTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
